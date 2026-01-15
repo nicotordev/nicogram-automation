dbfaec7724c4b5818833adb7d713fe5afbb0e9f5
Nicolas Torres <nicotordev@gmail.com>
2026-01-15 15:40:11 -0300

    feat: remove prisma cache

--- apps/api/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { consola } from "consola";
-import { startServer } from "./server/server.js";
-
-// Start the server
-startServer(3000);
-
-consola.box("Server started. Open http://localhost:3000 to control.");
